2dc45ee0efe02139baae027a81483932d72ecdf9
JaceDreams <jacedreams@gmail.com>
2021-04-22 21:23:05 -0400

    Progress

---
+++ Assets/_Labs/05-Audio/MouseAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void PlayClip(AudioClip clip) 
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
