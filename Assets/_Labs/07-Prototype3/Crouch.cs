ccb29d72466e7217a85be1847ac6e02c99061577
JaceDreams <jacedreams@gmail.com>
2021-05-03 19:58:31 -0400

    Prototype Update

---
+++ Assets/_Labs/07-Prototype3/Crouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public CharacterController m_characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            m_characterController.height = 0.9f;
+        }
+        else
+        {
+            m_characterController.height = 1.8f;
+        }
+    }
+}
