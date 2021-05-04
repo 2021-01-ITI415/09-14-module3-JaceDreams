961bb837fd3083c85bca395cb271d31990c63420
JaceDreams <jacedreams@gmail.com>
2021-05-03 20:22:30 -0400

    Added SFX for PickUp

---
+++ Assets/_Labs/07-Prototype3/ScoringSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectedSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectedSound.Play();
+        theScore += 1;
+        scoreText.GetComponent<Text>().text = "RINGS: " + theScore;
+        Destroy(gameObject);
+    }
+}
